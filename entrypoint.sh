49b5ee452fa5c9fbca1c60a6e55a773a8fbedd61
dberstein-broadcom <75124930+dberstein-broadcom@users.noreply.github.com>
2021-04-22 18:23:31 +0300

    cleanup

--- entrypoint.sh
+++ entrypoint.sh
@@ -9,13 +9,16 @@ git config --global user.name "Bump-N-Tag App"
 [ "x${GITHUB_EVENT_NAME}x" = "xpushx" ] || { GITHUB_REF=${GITHUB_HEAD_REF} && git checkout ${GITHUB_REF}; }
 
 # bump version ...
-N=3 && FILE="${1:-VERSION}" && BUMP_A=$(cat "${FILE}") && TMPFILE="$(mktemp version.XXXXXX)" \
-&& ( tr . \\n | sed -r $N' s/^(.*)([0-9]+)(.*)/echo \1$(echo 1+\2 | bc)\3/ge' && seq 1 $N ) \
-< "${FILE}" | head -n3 | tr \\n . | sed 's/\.*$/\n/g' \
-> "$TMPFILE" && mv "$TMPFILE" "${FILE}" && BUMP_B=$(cat "${FILE}") && echo -e "Bumped: ${BUMP_A} -> ${BUMP_B}"
+N=3
+FILE="${1:-VERSION}"
+BUMP_A=$(cat "${FILE}") && TMPFILE="$(mktemp version.XXXXXX)"
+( tr . \\n | sed -r $N' s/^(.*)([0-9]+)(.*)/echo \1$(echo 1+\2 | bc)\3/ge' && seq 1 $N ) \
+< "${FILE}" | head -n $N | tr \\n . | sed 's/\.*$/\n/g' \
+> "${TMPFILE}" && mv "$TMPFILE" "${FILE}" \
+&& BUMP_B=$(cat "${FILE}") && echo "Bumped: ${BUMP_A} -> ${BUMP_B}"
 
 # git commit ...
-git add "${FILE}" && git commit -m "Incremented to ${BUMP_B}"  -m "[skip ci]"
+git add "${FILE}" && git commit -m "Incremented to ${BUMP_B}" -m "[skip ci]"
 
 ( [ -n "$2" ] && [ "$2" = "true" ] ) && ( git tag -a "${BUMP_B}" -m "[skip ci]" ) || echo "No tag created"
 
